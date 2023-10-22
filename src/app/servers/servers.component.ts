782f7ea8cceb89fe5bf654645a3d5c1482783a8e
kaory-china <kaory_china@hotmail.com>
2023-10-21 22:33:41 -0300

    data binding hands on

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,36 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // defining selector as element
+
   selector: 'app-servers',
-
-  // defining selector as attribute
-  //selector: '[app-servers]',
-
-  // defining selector as class
-  //selector: '.app-servers',
-
-  // defining external html template
   templateUrl: './servers.component.html',
-
-  // inline html template, simples quotes for oneline and backticks for multiline
-  //template: `
-  //<app-server>
-  //</app-server>
-  //`,
-  
-  // defining external stylesheet
   styleUrls: ['./servers.component.css']
-
-  // defining inline styling
-  //styles: [`
-  //h3 {
-  //  color: red;
-  //}`, 
-  //`div {
-  //  color: red;
-  //}`]
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = true;
+  serverCreationStatus = "No server was created."
+  serverName = 'Server Name';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server: " + this.serverName + " was created successfully!";
+  }
+
+  //onUpdateServerName(event: Event) {
+    //console.log(event);
+    //this.serverName = (<HTMLInputElement>event.target).value;
+  //}
 
 }
